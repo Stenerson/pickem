513844ec237f26dc45c90aaf1e5c6b96300507b8
Matt Stenerson <stenerson.matt@gmail.com>
2015-10-16 11:45:52 -0500

    Rearrange files

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 var PicksApp = require('./components/PicksApp.jsx');
 var PicksAppAPIUtils = require('./utils/PicksAppAPIUtils');
-var PicksAppData = require('./PicksAppData');
 var React = require('react');
 window.React = React; // export for http://fb.me/react-devtools
 var ReactDOM = require('react-dom');
